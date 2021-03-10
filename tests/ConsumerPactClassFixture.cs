9fab6736cd75c63d75b194a27b99eef909e30440
David Simpson <David.simpson@footasylum.com>
2021-03-10 08:57:06 +0000

    Updated Pact name & Pactflow URL

--- tests/ConsumerPactClassFixture.cs
+++ tests/ConsumerPactClassFixture.cs
@@ -29,8 +29,8 @@ namespace tests
 
             PactBuilder = new PactBuilder(pactConfig);
 
-            PactBuilder.ServiceConsumer("pactflow-example-consumer-dotnet")
-                       .HasPactWith("pactflow-example-provider-dotnet");
+            PactBuilder.ServiceConsumer("consumer-in-dotnet")
+                       .HasPactWith("provider-in-dotnet");
 
             MockProviderService = PactBuilder.MockService(MockServerPort);
         }
